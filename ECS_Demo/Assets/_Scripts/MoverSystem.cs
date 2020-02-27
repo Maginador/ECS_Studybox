731f9b4feb565bf9baf932c031192b568acfabe7
Ricardo Costa Maginador <ricardo.cm@SW-RICARDO.local>
2020-02-27 14:23:12 -0400

    Initial ECS Demo with 3D objects

---
+++ ECS_Demo/Assets/_Scripts/MoverSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class MoverSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref MoveSpeedComponent speed) =>
+            {
+                translation.Value.x += speed.moveSpeedX * Time.DeltaTime;
+                if (translation.Value.x > 20f || translation.Value.x < -20)
+                    speed.moveSpeedX *= -1;
+                
+                translation.Value.z += speed.moveSpeedZ * Time.DeltaTime;
+                if (translation.Value.z > 10f || translation.Value.z < -50)
+                    speed.moveSpeedZ *= -1;
+
+            }
+            
+            );
+    }
+}
---
+++ ECS_Demo/Assets/_Scripts/Testing.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using Random = UnityEngine.Random;
+
+public class Testing : MonoBehaviour
+{
+
+    [SerializeField] private Mesh myMesh;
+    [SerializeField] private Material myMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager entityManager = World.Active.EntityManager;
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(MoveSpeedComponent),
+            
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+            );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(100, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype,entityArray);
+
+        for (int i = 0; i < entityArray.Length; i++)
+        {
+            
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelComponent{ level = Random.Range(10,20)});
+            entityManager.SetComponentData(entity, new MoveSpeedComponent{ moveSpeedX = Random.Range(3,5), moveSpeedZ = Random.Range(3,5)});
+            entityManager.SetComponentData(entity, new Translation{ Value = 
+                new float3(Random.Range(-20,20), 0, Random.Range(-50,10))});
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = myMesh,
+                material = myMaterial,
+            });
+        }
+
+        entityArray.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
